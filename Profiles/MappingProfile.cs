5478ed57e3e0a7eddd300f0a2259832a0d43fb0b
Henry <henrichukwudi@gmail.com>
2022-10-03 09:32:30 +0100

    Added automapping to the project

---
+++ Profiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Entities;
+using Shared.DataTransferObjects;
+
+namespace PayrollApi.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>()
+                .ForCtorParam("FullAddress",
+         //.ForMember(c => c.FullAddress,
+         opt => opt.MapFrom(x => string.Join(' ', x.Address, x.EmergencyContact)));
+        }
+    }
+}
